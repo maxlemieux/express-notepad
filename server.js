b9b5499de5534b87ee7bd8766fd71cfddd5ec30a
Max Lemieux <max.lemieux@gmail.com>
2020-03-29 18:05:20 -0700

    added start and watch scripts

---
+++ server.js
@@ -0,0 +1,59 @@
+// Dependencies
+// =============================================================
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/notes', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+// Displays all notes
+app.get('/api/notes', function(req, res) {
+  return res.json(notes);
+});
+
+// Create New Notes
+app.post('/api/notes', function(req, res) {
+  const newCharacter = req.body;
+  characters.push(newCharacter);
+  res.json(newCharacter);
+});
+
+// Displays a single character, or returns false
+app.get('/api/notes/:id', function(req, res) {
+  const note = req.params.note;
+
+  console.log(note);
+
+  for (const i = 0; i < notes.length; i++) {
+    if (note === notes[i].id) {
+      return res.json(notes[i]);
+    }
+  }
+
+  return res.json(false);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log('App listening on PORT ' + PORT);
+});
